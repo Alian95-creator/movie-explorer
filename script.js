f6dd76012059831885b0a3b3a367eb1fe34a50db
Alifian <alifiancandra93@gmail.com>
2026-03-16 21:46:21 +0700

    feat: setup tmdb api configuration

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ const BASE_URL = "https://api.themoviedb.org/3";
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
 const movieGrid = document.getElementById("movieGrid")
+
+async function getTrendingMovies(){
+
+const url = `${BASE_URL}/trending/movie/week?api_key=${API_KEY}`;
+
+const res = await fetch(url);
+
+const data = await res.json();
+
+displayMovies(data.results);
+
+}
